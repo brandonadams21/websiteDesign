95a1689602a8a90b2e066944912a751453a9cd5e
brandonadams21 <branmuffin32@icloud.com>
2023-12-13 23:27:17 -0600

    updated hero section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,27 +1,55 @@
 import React from "react";
-import { Card, Row, Col, Carousel } from "antd";
+import { Card, Row, Col } from "antd";
 import "../styles/Experience.css"; // Create a CSS file for styling
 
 const { Meta } = Card;
 
 const Experience = () => {
+  const cardStyle = {
+    // width: '80%', // Set the width as per your preference
+    marginLeft: '15%',
+    marginRight: '15%',
+    justifyContent: 'center',
+    
+  };
+
   return (
     <div className="ourWork">
-      <h2>Projects</h2>
-      <div className="card-container">
-        <Card hoverable cover={<img alt="example" src="/images/sat_img.png" />}>
-          <Meta
-            title="SAT by VK Integrated Systems"
-            description="https://sat.vkintsys.com"
-          />
-        </Card>
-        <Card hoverable cover={<img alt="example" src="/images/sat_img.png" />}>
-          <Meta title="TAK Stack" description="www.instagram.com" />
-        </Card>
-        <Card hoverable cover={<img alt="example" src="/images/sat_img.png" />}>
-          <Meta title="MARS" description="Mars" />
-        </Card>
-      </div>
+      <h2 style={{ fontSize: "56px" }}>Projects</h2>
+      <Row gutter={16}>
+        {/* Add gutter for spacing between columns */}
+        <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+          {/* Define column widths for responsiveness */}
+          <Card
+            hoverable
+            cover={<img alt="example" src="/images/sat_img.png" />}
+            style={cardStyle}
+          >
+            <Meta
+              title="SAT by VK Integrated Systems"
+              description="https://sat.vkintsys.com"
+            />
+          </Card>
+        </Col>
+        <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+          <Card
+            hoverable
+            cover={<img alt="example" src="/images/sat_img.png" />}
+            style={cardStyle}
+          >
+            <Meta title="TAK Stack" description="www.instagram.com" />
+          </Card>
+        </Col>
+        <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+          <Card
+            hoverable
+            cover={<img alt="example" src="/images/sat_img.png" />}
+            style={cardStyle}
+          >
+            <Meta title="MARS" description="Mars" />
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
