13407afe7d4da01f2194d6d0d561692e79eef708
brandonadams21 <branmuffin32@icloud.com>
2023-12-13 23:27:17 -0600

    updated hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,20 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "../styles/heroSection.css";
 import { ThemeContext } from "./ThemeContext";
+import Typewriter from "./Typewriter";
 
 const HeroSection = () => {
-
   const { theme, setTheme } = useContext(ThemeContext);
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
+    setTheme(theme === "light" ? "dark" : "light");
   };
 
   return (
     <div className="heroSection">
-      <h1 className="header">Brandon Adams</h1>
-      {/* <p className="subheader">Your welding solution</p> */}
-      <button className="ctaButton" onClick={toggleTheme}>{theme === 'light' ? 'Modern' : 'Retro'}</button>
+      <h1 className="header">
+        <Typewriter text="Dive into the Digital Dystopia" delay={100} />
+      </h1>
+      <button className="brk-btn" onClick={toggleTheme}>
+        {theme === "light" ? "Modern" : "Retro"}
+      </button>
     </div>
   );
 };
